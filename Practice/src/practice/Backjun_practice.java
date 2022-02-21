9d6beee92e8acd5a4b21c83d349a7105a85ad359
DESKTOP-NUOPUIF\ITPS <ITPS@gmail.com>
2022-02-21 14:22:38 +0900

    백준 연습

---
+++ Practice/src/practice/Backjun_practice.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class Backjun_practice {
+
+	public static void main(String[] args) {
+		
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+		
+		
+	}
+
+}
